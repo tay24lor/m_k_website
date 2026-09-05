455205bedf000b44e051ce30d8a5d0786554b132
tay24lor <69598097+tay24lor@users.noreply.github.com>
2026-09-04 22:31:31 -0500

    fix

--- my-flower-store/vite.config.ts
+++ my-flower-store/vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.GITHUB_PAGES_BASE ?? '/m_k_website/my-flower-store/',
-  plugins: [react()],
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
