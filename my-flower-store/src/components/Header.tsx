7c6c371ab16252a10792a091158e3c4b5e93b452
tay24lor <69598097+tay24lor@users.noreply.github.com>
2026-09-13 20:23:08 -0500

    [UPDATE] Name changed in header

--- my-flower-store/src/components/Header.tsx
+++ my-flower-store/src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
     return (
         <header className="site-header">
             <div className="brand-lockup">
-                <p className="brand-mark">M|K</p>
+                <p className="brand-mark">Fox Florals</p>
                 <p className="brand-tagline">Custom Resin Floral Preservation</p>
             </div>
         </header>
